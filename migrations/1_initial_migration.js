58dd4dbc51a6cdfcb62cc3b64c6fb59d32352856
dangdang1ing15 <shine_0512@naver.com>
2021-09-29 11:54:44 +0900

    migrations set

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+const Migrations = artifacts.require("SimpleVoting");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(SimpleVoting);
 };
